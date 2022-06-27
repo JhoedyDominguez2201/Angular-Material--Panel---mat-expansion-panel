9a48daab0ef4d3761ec546990cb2c5a28dae1774
JhoedyDominguez2201 <l17390333@chetumal.tecnm.mx>
2022-06-26 23:38:50 -0500

    Commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PostblogService } from './postblog.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto036';
+export class AppComponent implements OnInit {
+
+  post:any;
+
+  constructor(private postblogService: PostblogService) { }
+
+  ngOnInit() {
+    this.postblogService.retornar()
+      .subscribe(result => this.post = result)
+  }
 }
